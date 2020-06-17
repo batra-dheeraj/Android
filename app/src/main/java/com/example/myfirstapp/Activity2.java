1bab6d8ebec669c9ef4108b0ab1b98b2e4158029
dheerajbatra <batradheeraj1998@gmail.com>
2020-06-17 14:34:04 +0530

    Learning Activities

---
+++ app/src/main/java/com/example/myfirstapp/Activity2.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    public void prevActivity(View v){
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
